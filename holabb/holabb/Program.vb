b33ebdedbaee8c6be27412e66676f9d17d14681b
Gollumkanzi <160975895+Gollumkanzi@users.noreply.github.com>
2024-02-29 16:08:57 -0700

    codigo 29/02/24

---
+++ holabb/holabb/Program.vb
@@ -0,0 +1,22 @@
+Imports System
+
+Module Program
+    Sub Main()
+
+
+        Dim pesomuestra As Integer
+        Dim RB As Single
+        Dim VT As Single
+
+        Console.Write(" INGRESE PESO MUESTRA :")
+        pesomuestra = Console.ReadLine()
+        Console.Write("INGRESE RB :")
+        RB = Console.ReadLine()
+        VT = pesomuestra * RB
+        Console.Write(" EL VOLUMEN TOTAL ES :")
+        Console.Write(VT)
+        Console.ReadKey()
+
+
+    End Sub
+End Module
