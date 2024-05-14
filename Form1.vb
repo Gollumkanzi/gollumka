729febd9d33d620c27b2000477fc093e8ebb5789
Gollumkanzi <160975895+Gollumkanzi@users.noreply.github.com>
2024-05-13 17:07:16 -0700

    Add files via upload

--- Form1.vb
+++ Form1.vb
@@ -1,90 +1,139 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim metros, M, V, N, M1, V1, N1, MR, VR, NR, op, rollos As Double
-        Dim err As Integer
-        Dim dia, dia2, res3 As Double
-        Dim hr, hr2, res2 As Double
-        Dim npxm As Double
-        Dim dhilo As Double
-        Dim tiem, tiem2, res1 As Double
-        Dim k As Double
+        Dim valor1 As Integer
+        Dim Nm As Integer
+
+        valor1 = TextBox1.Text
+
+
+        Nm = valor1 * 0.5906
+        Label1.Text = Nm
 
 
 
-        metros = TextBox1.Text
-        err = TextBox2.Text
-
-        npxm = 300
-        dhilo = 0.00015
-        k = 1.2
-
-        If err < 10 Then
-            MsgBox("El margen de error es muy pequeño")
-        ElseIf err >= 10 And err < 20 Then
-            tiem = metros / (npxm * dhilo * k)
-            hr = tiem / 60
-            dia = hr / 24
-
-            Label3.Text = "El tiempo en minutos es: " & tiem
-            Label4.Text = "El tiempo horas es: " & hr
-            Label5.Text = "El tiempo dia por los 3 turnos es: " & dia
-            op = metros / 24
-            M1 = metros * 0.9
-            V1 = 0
-            N1 = 0
-
-            Label6.Text = "En el turno matutino se hace " & M1 & " metros con el porcentaje de error del 10% "
-            Label7.Text = "En el turno vespertino se hace " & V1 & " metros"
-            Label8.Text = "En el turno nocturno se hace " & N1 & " metros"
-            rollos = metros / 30
-            Label9.Text = "El numero de rollos son " & rollos
-
-        ElseIf err >= 20 And err < 30 Then
-            tiem = metros / (npxm * dhilo * k)
-            hr = tiem / 60
-            dia = hr / 24
-
-            Label3.Text = "El tiempo en minutos es: " & tiem
-            Label4.Text = "El tiempo horas es: " & hr
-            Label5.Text = "El tiempo dia por los 3 turnos es: " & dia
-
-            op = metros / 17
-            N1 = op * 8
-            V1 = op * 9
 
 
-            Label6.Text = "En el turno matutino se hace " & N1 & " metros con el porcentaje de error del 10% "
-            Label7.Text = "En el turno vespertino se hace " & V1 & " metros"
-            Label8.Text = "En el turno nocturno se hace " & 0 & " metros"
-
-            rollos = metros / 30
-            Label9.Text = "El numero de rollos son " & rollos
-
-        ElseIf err >= 30 And err < 40 Then
-            tiem = metros / (npxm * dhilo * k)
-            hr = tiem / 60
-            dia = hr / 24
-
-            Label3.Text = "El tiempo en minutos es: " & tiem
-            Label4.Text = "El tiempo horas es: " & hr
-            Label5.Text = "El tiempo dia por los 3 turnos es: " & dia
-
-            op = metros / 24
-            M1 = op * 8
-            V1 = op * 9
-            N1 = op * 7
-
-            Label6.Text = "En el turno matutino se hace " & M1 & " metros con el porcentaje de error del 10% "
-            Label7.Text = "En el turno vespertino se hace " & V1 & " metros"
-            Label8.Text = "En el turno nocturno se hace " & N1 & " metros"
-
-            rollos = metros / 30
-            Label9.Text = "El numero de rollos son " & rollos
-
-        End If
     End Sub
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim valor1 As Integer
+        Dim den As Integer
 
+        valor1 = TextBox2.Text
+
+
+        den = 5315 / valor1
+        Label3.Text = den
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim valor1 As Integer
+        Dim tex As Integer
+
+        valor1 = TextBox3.Text
+
+
+        tex = 590.6 / valor1
+        Label4.Text = tex
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim valor1 As Integer
+        Dim nec As Integer
+
+        valor1 = TextBox4.Text
+
+
+        nec = valor1 * 1
+        Label5.Text = nec
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim valor1 As Integer
+        Dim nec As Integer
+
+        valor1 = TextBox5.Text
+
+
+        nec = valor1 * 0.5906
+        Label2.Text = nec
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim valor1 As Integer
+        Dim nm As Integer
+
+        valor1 = TextBox6.Text
+
+
+        nm = valor1 * 1
+        Label6.Text = nm
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim valor1 As Integer
+        Dim dem As Integer
+
+        valor1 = TextBox7.Text
+
+
+        dem = 9000 / valor1
+        Label7.Text = dem
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Dim valor1 As Integer
+        Dim tex As Integer
+
+        valor1 = TextBox8.Text
+
+
+        tex = 1000 / valor1
+        Label8.Text = tex
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Dim valor1 As Integer
+        Dim nec As Integer
+
+        valor1 = TextBox9.Text
+
+
+        nec = 5315 / valor1
+        Label9.Text = nec
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Dim valor1 As Integer
+        Dim nm As Integer
+
+        valor1 = TextBox10.Text
+
+
+        nm = 9000 / valor1
+        Label10.Text = nm
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Dim valor1 As Integer
+        Dim dem As Integer
+
+        valor1 = TextBox11.Text
+
+
+        dem = 1 * valor1
+        Label11.Text = dem
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Dim valor1 As Integer
+        Dim tex As Integer
+
+        valor1 = TextBox12.Text
+
+
+        tex = valor1 * 9
+        Label12.Text = tex
     End Sub
 End Class
